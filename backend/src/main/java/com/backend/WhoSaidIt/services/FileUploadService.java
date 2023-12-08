54e002a13427fba87f80d57de397b070ecf0f118
Nathan Raymant <nathan.raymant@mail.utoronto.ca>
2023-12-08 16:15:26 -0500

    Updated file parsing algorithm to be compatible with older chat history export formatting

--- backend/src/main/java/com/backend/WhoSaidIt/services/FileUploadService.java
+++ backend/src/main/java/com/backend/WhoSaidIt/services/FileUploadService.java
@@ -38,6 +38,11 @@ public class FileUploadService {
         this.messageService = messageService;
     }
 
+    // Indices of the date, sender, and content in the parsed line
+    private static final int DATE_INDEX = 0;
+    private static final int SENDER_INDEX = 1;
+    private static final int CONTENT_INDEX = 2;
+
     // This method is meant to filter out messages that are not noteworthy.
     // For now, we will only filter based on the length of the message.
     // Longer messages are more likely to be attributable to a specific person.
@@ -52,25 +57,56 @@ public class FileUploadService {
     // Returns a string array of length 3
     // [0]: Date string, [1]: Sender name, [2]: Message content
     private static String[] parseLine(String line, int minCharacters) {
-        // The following regex matches the format:
-        // MM/DD/YY, HH:MM AM/PM - Sender Name: Message Content
-        Pattern p = Pattern.compile("^\\d{1,2}/\\d{1,2}/\\d{1,2}, \\d{1,2}:\\d{1,2}[ \\u202F](AM|PM) - .*:.*$");
-        if (!p.matcher(line).matches()) {
+        // Pattern A: "MM/DD/YY, HH:MM AM/PM - Sender Name: Message Content"
+        Pattern a = Pattern.compile("^\\d{1,2}/\\d{1,2}/\\d{1,2}, \\d{1,2}:\\d{1,2}[ \\u202F](AM|PM) - .*:.*$");
+        // Pattern B (older export version): "[MM/DD/YY, HH:MM:SS AM/PM] Sender Name: Message Content"
+        Pattern b = Pattern.compile("^\\[\\d{1,2}/\\d{1,2}/\\d{1,2}, \\d{1,2}:\\d{1,2}:\\d{1,2} (AM|PM)] .*: .*$");
+
+        String[] parsedLine;
+        if (a.matcher(line).matches()) {
+            parsedLine = parsePatternA(line);
+        } else if (b.matcher(line).matches()) {
+            parsedLine = parsePatternB(line);
+        } else {
             return null;
         }
+
+        return passesFilter(parsedLine[CONTENT_INDEX], minCharacters) ? parsedLine : null;
+    }
+
+    // Pattern A: "MM/DD/YY, HH:MM AM/PM - Sender Name: Message Content"
+    private static String[] parsePatternA(String line) {
         String[] parsedLine = new String[3];
         int dateEndIndex = -1;
         for (int i = 0; i < line.length(); i++) {
             if (line.charAt(i) == '-' && dateEndIndex == -1) {
-                parsedLine[0] = line.substring(0, i - 1);
+                parsedLine[DATE_INDEX] = line.substring(0, i - 1);
                 dateEndIndex = i + 2;
             } else if (line.charAt(i) == ':' && dateEndIndex != -1) {
-                parsedLine[1] = line.substring(dateEndIndex, i);
-                parsedLine[2] = line.substring(i + 2);
+                parsedLine[SENDER_INDEX] = line.substring(dateEndIndex, i);
+                parsedLine[CONTENT_INDEX] = line.substring(i + 2);
                 break;
             }
         }
-        return passesFilter(parsedLine[2], minCharacters) ? parsedLine : null;
+        return parsedLine;
+    }
+
+    // Pattern B (older export version): "[MM/DD/YY, HH:MM:SS AM/PM] Sender Name: Message Content"
+    private static String[] parsePatternB(String line) {
+        String[] parsedLine = new String[3];
+        int dateEndIndex = -1;
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == ']' && dateEndIndex == -1) {
+                // We exclude the seconds from the older version of the date string
+                parsedLine[DATE_INDEX] = line.substring(1, i - 6) + line.substring(i - 3, i);
+                dateEndIndex = i + 2;
+            } else if (line.charAt(i) == ':' && dateEndIndex != -1) {
+                parsedLine[SENDER_INDEX] = line.substring(dateEndIndex, i);
+                parsedLine[CONTENT_INDEX] = line.substring(i + 2);
+                break;
+            }
+        }
+        return parsedLine;
     }
 
     // Converts a date string of the form "MM/DD/YYYY, HH:MM AM/PM" to a LocalDateTime object
@@ -89,19 +125,20 @@ public class FileUploadService {
         );
         GroupChat groupChat = groupChatService.createGroupChat(user, groupChatName, file.getOriginalFilename());
 
+        // Each key in the messages map is a sender name, and each value is a list of pairs of timestamps and message
         HashMap<String, List<Pair<LocalDateTime, String>>> messages = new HashMap<String, List<Pair<LocalDateTime, String>>>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
         while (reader.ready()) {
             String[] parsedLine = parseLine(reader.readLine(), minCharacters);
             if (parsedLine == null) { continue; }
 
-            String senderName = parsedLine[1];
-            String messageContent = parsedLine[2];
-            LocalDateTime timestamp = convertDateString(parsedLine[0]);
+            String senderName = parsedLine[SENDER_INDEX];
+            String messageContent = parsedLine[CONTENT_INDEX];
+            LocalDateTime timestamp = convertDateString(parsedLine[DATE_INDEX]);
             if (messages.containsKey(senderName)) {
-                messages.get(senderName).add(new Pair<LocalDateTime, String>(timestamp, messageContent));
+                messages.get(senderName).add(new Pair<>(timestamp, messageContent));
             } else {
-                messages.put(senderName, new ArrayList<>(List.of(new Pair<LocalDateTime, String>(timestamp, messageContent))));
+                messages.put(senderName, new ArrayList<>(List.of(new Pair<>(timestamp, messageContent))));
             }
         }
 
