b160e7cd5ea2112ca7043e1b674fdf2354e8ab76
Nathan Raymant <nathan.raymant@mail.utoronto.ca>
2024-01-11 23:14:34 -0500

    Implemented state management system for quizzes and relevant game logic

--- frontend/app/(pages)/quiz/[...query]/page.tsx
+++ frontend/app/(pages)/quiz/[...query]/page.tsx
@@ -3,12 +3,38 @@
 import useValidateUrlToken from "@/app/hooks/security/useValidateUrlToken";
 import useAuth from "@/app/hooks/security/useAuth";
 
-import { SurvivalQuizInfo, TimeAttackQuizInfo, Participant } from "@/app/interfaces";
+import { SurvivalQuizInfo, TimeAttackQuizInfo, Message, Participant } from "@/app/interfaces";
 
 import useGetQuizInfo from "@/app/hooks/api_access/quizzes/useGetQuizInfo";
+import useGetRandomQuizMessage from "@/app/hooks/api_access/messages/useGetRandomQuizMessage";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
+
+// The page state is used to determine what to render on the level of the page.
+// Transitional states are used for animations, etc.
+type PageState = "LANDING" | "QUIZ_STARTING" | "QUIZ" | "QUIZ_ENDING" | "RESULTS";
+type PageStateReducer = (state: PageState, action: PageStateReducerAction) => PageState;
+interface PageStateReducerAction {
+    type: PageStateReducerActionType;
+}
+type PageStateReducerActionType = "init_start_quiz" | "start_quiz" | "init_end_quiz" | "end_quiz";
+
+// The game state is used to determine what to render on the level of the game.
+type GameState = "QUESTION_STARTING" | "QUESTION" | "CORRECT_ANSWER" | "INCORRECT_ANSWER";
+type GameStateReducer = (state: GameState, action: GameStateReducerAction) => GameState;
+interface GameStateReducerAction {
+    type: GameStateReducerActionType;
+}
+type GameStateReducerActionType = "init_next_question" | "next_question" |  "correct_answer" | "incorrect_answer";
+
+// Custom type guards
+const isTimeAttackQuizInfo = (quizInfo: TimeAttackQuizInfo | SurvivalQuizInfo): quizInfo is TimeAttackQuizInfo => {
+    return quizInfo.type === "TIME_ATTACK";
+}
+const isSurvivalQuizInfo = (quizInfo: TimeAttackQuizInfo | SurvivalQuizInfo): quizInfo is SurvivalQuizInfo => {
+    return quizInfo.type === "SURVIVAL";
+}
 
 export default function Quiz({ params }: { params: { query: string[] } }) {
 
@@ -21,36 +47,63 @@ export default function Quiz({ params }: { params: { query: string[] } }) {
 
     // API access hooks
     const getQuizInfo = useGetQuizInfo();
+    const getRandomQuizMessage = useGetRandomQuizMessage();
 
     // Security
     const { auth } = useAuth();
     const validateUrlToken = useValidateUrlToken();
+    // This is the jwt that is used to authenticate the user if they are not logged in. It is retrieved from the urlToken.
+    const [shareableToken, setShareableToken] = useState<string | null>(null);
 
     // ----------- State (Data) -----------
     const [quizInfo, setQuizInfo] = useState<TimeAttackQuizInfo | SurvivalQuizInfo | null>(null);
+    const [currentMessage, setCurrentMessage] = useState<Message | null>(null);
+    const [nextMessage, setNextMessage] = useState<Message | null>(null);
+    const [seenMessageIds, setSeenMessageIds] = useState<Array<number>>([]);
+
+    // ----------- State (Game/UI) -----------
+    const [score, setScore] = useState<number>(0); // For Survival Quizzes, this state records the streak instead.
+    const [scoreGained, setScoreGained] = useState<number>(0); // The score gained for a specific question
+    // The options for the current message. Max 4 options, including the correct answer.
+    const [participantOptions, setParticipantOptions] = useState<Array<Participant>>([]);
+
+    // Timing
+    const [quizStartTime, setQuizStartTime] = useState<Date>(new Date());
+    const [questionStartTime, setQuestionStartTime] = useState<Date>(new Date());
+    const [totalTimeTaken, setTotalTimeTaken] = useState<number | null>(null); // In milliseconds
 
     // ----- Data Retrieval/Authentication -----
     useEffect(() => {
         const getPageData = async () => {
             
-            // If the user is not logged in, we need to authenticate them using the urlToken
+            // If the user is not logged in, we need to extract the urlToken and authenticate them
             let shareableToken: string | null = null;
             if (urlToken) {
                 shareableToken = await validateUrlToken(quizId, urlToken);
                 if (!shareableToken && !auth) {
-                    // If the urlToken is invalid, redirect to the home page
+                    // If the urlToken is invalid (and user is not logged in), redirect to the home page
                     console.error("Error authenticating user, redirecting to root");
                     router.push("/");
                     return;
+                } else {
+                    setShareableToken(shareableToken);
                 }
             }
 
-            // Once authentication is verified, retrieve the static data
+            // Once authentication is verified, retrieve the static data and the first two messages
             const quiz: TimeAttackQuizInfo | SurvivalQuizInfo | null = await getQuizInfo(quizId, shareableToken || undefined);
-            // const groupChat: GroupChat | null = await getGroupChat(groupChatId, shareableToken || undefined);
-            // const participants: Array<Participant> | null = await getParticipants(groupChatId, shareableToken || undefined);
-            if (quiz) {
+            const currentMessage: Message | null = await getRandomQuizMessage(quizId, [], shareableToken || undefined);
+            if (quiz && currentMessage) {
                 setQuizInfo(quiz);
+                setCurrentMessage(currentMessage);
+                setParticipantOptions(generateParticipantOptions(currentMessage.sender));
+                let excludedMessageIds = [currentMessage.id];
+                const nextMessage: Message | null = await getRandomQuizMessage(quizId, excludedMessageIds, shareableToken || undefined);
+                if (nextMessage) {
+                    setNextMessage(nextMessage);
+                    excludedMessageIds.push(nextMessage.id);
+                }
+                setSeenMessageIds(excludedMessageIds);
             } else {
                 console.error("Error retrieving data, redirecting to root");
                 router.push("/");
@@ -58,6 +111,131 @@ export default function Quiz({ params }: { params: { query: string[] } }) {
         }
         getPageData();
     }, []);
+
+    // ----- Data Helpers -----
+    const getNextMessage = async () => {
+        if (nextMessage) setCurrentMessage(nextMessage);
+        setParticipantOptions(generateParticipantOptions(currentMessage?.sender));
+        const newNextMessage: Message | null = await getRandomQuizMessage(quizId, seenMessageIds, shareableToken || undefined);
+        if (newNextMessage) {
+            setNextMessage(newNextMessage);
+            setSeenMessageIds([...seenMessageIds, newNextMessage.id]);
+        } else {
+            // If there are no more messages, end the quiz
+            pageStateDispatch({ type: "init_end_quiz" });
+        }
+    }
+
+    // Gets a random selection of 4 participants to choose from, including the correct participant.
+    const generateParticipantOptions = (currentSender: Participant | undefined): Array<Participant> => {
+        if (!quizInfo || !currentSender) return [];
+        let potentialParticipants: Array<Participant> = quizInfo.participants.filter(p => p.id !== currentSender.id);
+        const options = [currentSender];
+        while (options.length < Math.min(4, potentialParticipants.length)) {
+            const randomParticipant = potentialParticipants[Math.floor(Math.random() * potentialParticipants.length)];
+            potentialParticipants = potentialParticipants.filter(p => p.id !== randomParticipant.id);
+            options.push(randomParticipant);
+        }
+        
+        // Shuffle the options
+        for (let i = options.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * i);
+            const temp = options[i];
+            options[i] = options[j];
+            options[j] = temp;
+        }
+
+        return options;
+    }
+
+    // ----- State Change Handlers/Reducers -----
+
+    // This reducer handles state changes specific to the page. I.e., transitions between the landing page, quiz, and results.
+    const pageStateReducer = (state: PageState, action: PageStateReducerAction): PageState => {
+        switch (action.type) {
+            case "init_start_quiz": // Animations: Fade out the landing page
+                setTimeout(() => {
+                    pageStateDispatch({ type: "start_quiz" });
+                }, 3000);
+                return "QUIZ_STARTING";
+
+            case "start_quiz":
+                const startTime = new Date();
+                setQuizStartTime(startTime);
+                setQuestionStartTime(startTime); // The first question starts at the same time as the quiz
+                return "QUIZ";
+
+            case "init_end_quiz": // Animations: Fade out the quiz
+                setTotalTimeTaken(Date.now() - quizStartTime.getTime());
+                setTimeout(() => {
+                    pageStateDispatch({ type: "end_quiz" });
+                }, 3000);
+                return "QUIZ_ENDING";
+
+            case "end_quiz":
+                return "RESULTS";
+
+            default: // This should never happen
+                return state;
+        }
+    }
+
+    // This reducer handles state changes specific to the game/quiz. I.e., correctness of answers, question transitions, etc.
+    const gameStateReducer = (state: GameState, action: GameStateReducerAction): GameState => {
+        if (!quizInfo || !currentMessage) return state;
+
+        switch (action.type) {
+            case "init_next_question": // Animations: Slide off the current message to reveal the next message; Fade out participant options
+                setTimeout(() => {
+                    gameStateDispatch({ type: "next_question" });
+                }, 1000);
+                return "QUESTION_STARTING";
+
+            case "next_question":
+                getNextMessage();
+                setQuestionStartTime(new Date());
+                setScoreGained(0);
+                return "QUESTION";
+
+            case "correct_answer":
+                if (isTimeAttackQuizInfo(quizInfo)) {
+                    const { initialQuestionScore, penaltyPerSecond } = quizInfo;
+                    const secondsTaken = Math.floor((Date.now() - questionStartTime.getTime()) / 1000); // Convert to seconds (rounded down)
+                    const qScore = initialQuestionScore - (secondsTaken * penaltyPerSecond);
+                    setScoreGained(qScore);
+                    setScore(score + qScore);
+                } else {
+                    setScore(score + 1);
+                }
+
+                setTimeout(() => {
+                    gameStateDispatch({ type: "init_next_question" });
+                }, 2000);
+                return "CORRECT_ANSWER";
+            
+            case "incorrect_answer":
+                if (isTimeAttackQuizInfo(quizInfo)) {
+                    const { wrongAnswerPenalty } = quizInfo;
+                    setScoreGained(-wrongAnswerPenalty);
+                    setScore(score - wrongAnswerPenalty);
+                } else {
+                    // In Survival Quizzes, an incorrect answer ends the quiz
+                    pageStateDispatch({ type: "init_end_quiz" });
+                }
+
+                setTimeout(() => {
+                    gameStateDispatch({ type: "init_next_question" });
+                }, 2000);
+                return "INCORRECT_ANSWER";
+
+            default: // This should never happen
+                return state;
+        }
+    }
+
+    // ----------- REDUCERS -----------
+    const [pageState, pageStateDispatch] = useReducer<PageStateReducer>(pageStateReducer, "LANDING");
+    const [gameState, gameStateDispatch] = useReducer<GameStateReducer>(gameStateReducer, "QUESTION");
 
     return (<>
         <div>Quiz Id: {quizId}</div>
---
+++ frontend/app/hooks/api_access/messages/useGetRandomQuizMessage.ts
@@ -0,0 +1,48 @@
+import { EXTERNAL_API_ROOT } from "@/app/constants";
+
+import useAuthFetch from "../../security/useAuthFetch";
+import { Message } from "@/app/interfaces";
+
+export default function useGetRandomQuizMessage() {
+
+    const authFetch = useAuthFetch();
+
+    const getRandomQuizMessage = async (
+        quizId: number, 
+        excludedMessageIds: Array<number>,
+        shareableToken?: string | undefined
+    ): Promise<Message | null> => {
+
+        const requestUrl: string = `${EXTERNAL_API_ROOT}/quizzes/${quizId}/messages/random`;
+        const searchParams = new URLSearchParams({ excludedMessageIds: excludedMessageIds.join(',') });
+
+        try {
+            const response: Response = await authFetch(requestUrl + searchParams, {}, shareableToken);
+            if (!response.ok) {
+                if (response.status >= 400 && response.status < 500) {
+                    console.error(`Client failed request: ${response.status}`);
+                    return null;
+                } else if (response.status >= 500) {
+                    console.error(`Server failed request: ${response.status}`);
+                    return null;
+                }
+            }
+
+            const parsedJson = await response.json();
+
+            // Converting the deserialized LocalDateTime message timestamps to Date objects
+            const message: Message = {
+                ...parsedJson,
+                timestamp: new Date(parsedJson.timestamp)
+            };
+
+            return message;
+        
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    return getRandomQuizMessage;
+}
