0397d157988db520945424e1a6f1c40e0bd547cc
Nathan Raymant <nathan.raymant@mail.utoronto.ca>
2024-01-14 00:08:23 -0500

    Fixed minor error with JSON inheritance type discrimination

--- backend/src/main/java/com/backend/WhoSaidIt/DTOs/quiz/info/QuizInfoDTO.java
+++ backend/src/main/java/com/backend/WhoSaidIt/DTOs/quiz/info/QuizInfoDTO.java
@@ -1,8 +1,6 @@
 package com.backend.WhoSaidIt.DTOs.quiz.info;
 
 import com.backend.WhoSaidIt.DTOs.ParticipantDTO;
-import com.backend.WhoSaidIt.DTOs.quiz.SurvivalQuizDTO;
-import com.backend.WhoSaidIt.DTOs.quiz.TimeAttackQuizDTO;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
@@ -10,8 +8,8 @@ import java.util.List;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = TimeAttackQuizDTO.class, name = "TIME_ATTACK"),
-        @JsonSubTypes.Type(value = SurvivalQuizDTO.class, name = "SURVIVAL")
+        @JsonSubTypes.Type(value = TimeAttackQuizInfoDTO.class, name = "TIME_ATTACK"),
+        @JsonSubTypes.Type(value = SurvivalQuizInfoDTO.class, name = "SURVIVAL")
 })
 public sealed interface QuizInfoDTO permits TimeAttackQuizInfoDTO, SurvivalQuizInfoDTO {
     Long id();
