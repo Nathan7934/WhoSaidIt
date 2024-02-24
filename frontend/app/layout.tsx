31685ff34d449454cb894d1e5b00cfffbdf806cc
Nathan Raymant <nathan.raymant@mail.utoronto.ca>
2024-02-24 16:00:51 -0500

    Minor tweak: Gave <html> bg-black, fixing <body> render cutoff on desktop

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="en" className='bg-black'>
             <body className={inter.className + " overflow-hidden"} data-theme="dark">
                 <Providers>
                     <NavBar />
