4d2c5b9deb16d9f4f166647ac25e5cc4d2828538
Nathan Raymant <nathan.raymant@mail.utoronto.ca>
2024-02-25 23:22:53 -0500

    Configured NextJS API url environment variables; Production uses public API

--- frontend/app/constants.ts
+++ frontend/app/constants.ts
@@ -1,11 +1,12 @@
 // Stores the external SpringBoot API root:
-export const EXTERNAL_API_ROOT: string = "http://localhost:8080/api";
+export const EXTERNAL_API_ROOT: string = `${process.env.EXTERNAL_API_URL}/api`;
+
 
 // Stores the internal NextJS API root:
 export const INTERNAL_API_ROOT: string = "http://localhost:3000/api";
 
 // Stores the domain URL:
-export const DOMAIN_URL: string = "http://localhost:3000";
+export const DOMAIN_URL: string = `${process.env.DOMAIN_URL}`;
 
 // How long the user must hold on a message to select it in mobile layout (ms)
 export const MOBILE_HOLD_DURATION = 400;
